852d396dcaf22b85df3f7970170f14cf7d17ec9f
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2014-06-20 11:28:32 +0200

    Loading config from file.

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var auth = require('./config.json').auth;
+var config = require('./config');
 
 var url = 'https://icemobile.atlassian.net/rest/api/latest/project';
 var callback = function(err, result, body) {
@@ -7,7 +7,9 @@ var callback = function(err, result, body) {
     console.log(body);
 };
 
+config.setCredentials('user', 'pass');
+
 request.get({
     url: url,
-    auth: auth
+    auth: config.getCredentials()
 }, callback);
